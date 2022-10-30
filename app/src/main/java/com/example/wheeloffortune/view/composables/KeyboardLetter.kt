89508179e0f4a892430378c411df0089330e6f8f
Mads Sørensen <s215805@student.dtu.dk>
2022-10-30 13:26:51 +0100

    added a composable button for a letter to be used when the user guesses a letter

---
+++ app/src/main/java/com/example/wheeloffortune/view/composables/KeyboardLetter.kt
@@ -0,0 +1,32 @@
+package com.example.wheeloffortune.view.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LetterButton (
+    letter: Char,
+    onButtonClick: () -> Unit
+) {
+    Box(modifier = Modifier.padding(6.dp, 0.dp).clickable { onButtonClick() }) {
+        Box(modifier = Modifier.background(Color.Gray)) {
+            Box (
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(10.dp, 10.dp)
+            ) {
+
+                Text(text = letter.toString(), Modifier.width(10.dp))
+            }
+        }
+    }
+}
