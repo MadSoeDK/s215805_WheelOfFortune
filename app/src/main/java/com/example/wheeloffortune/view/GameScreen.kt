90583b2afacc789cbd565c6ee670836c996f1686
Mads Sørensen <s215805@student.dtu.dk>
2022-11-07 10:49:02 +0100

    arranged UI elements

--- app/src/main/java/com/example/wheeloffortune/view/GameScreen.kt
+++ app/src/main/java/com/example/wheeloffortune/view/GameScreen.kt
@@ -1,10 +1,18 @@
 package com.example.wheeloffortune.view
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.wheeloffortune.GameViewModel
 import com.example.wheeloffortune.view.composables.LetterInput
@@ -13,24 +21,65 @@ import com.example.wheeloffortune.view.composables.LetterInput
 fun GameScreen(
     viewModel: GameViewModel
 ) {
-    Column {
-        Text(text = viewModel.gameState.lettersUsed)
-        Text(text = viewModel.gameState.lives.toString())
-        Text(text = viewModel.gameState.category.toString())
-        Text(text = viewModel.gameState.points.toString())
-        Text(text = viewModel.gameState.word)
-        Text(text = viewModel.gameState.guessedWord)
+    Column(
+        modifier = Modifier
+            .padding(10.dp, 30.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        if(!viewModel.gameWon) {
+            Column {
+                Column(
+                    modifier = Modifier
+                        .background(Color.LightGray)
+                        .fillMaxWidth()
+                        .padding(0.dp, 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = viewModel.category.toString())
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = viewModel.guessedWord, fontSize = 24.sp, letterSpacing = 5.sp)
+                }
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Score: " + viewModel.points.toString())
+                        Text(text = "Lives: " + viewModel.lives.toString())
+                    }
+                    Text(text = "Letters used: " + viewModel.lettersUsed)
+                }
+            }
 
-        Button(onClick = {  } ) {
-            Text(text = viewModel.getGuessedWord())
-        }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = viewModel.gameMessage,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(0.dp, 50.dp),
+                    textAlign = TextAlign.Center
+                )
 
-        if(viewModel.keyboard) {
-            LetterInput { viewModel.onLetterClick(it) }
-        }
-        
-        Button(onClick = { viewModel.spinWheel() }) {
-            Text(text = "Spin the wheel!")
+                if(viewModel.keyboard) {
+                    LetterInput(viewModel.lettersUsed) { viewModel.onLetterClick(it) }
+                } else {
+                    Button(onClick = { viewModel.spinWheel() }) {
+                        Text(text = "Spin the wheel!")
+                    }
+                }
+            }
+        } else {
+            Button(onClick = { viewModel.playAgain() }) {
+                Text(text = "You have won! Play again")
+            }
         }
     }
 }
